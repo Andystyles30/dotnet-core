a71a029a29c85ce06d572884adf6d4e03007e5e1
Andy Styles <andystyles30@gmail.com>
2021-02-05 15:23:58 +0000

    implement serviceResponse

--- Controllers/Services/CharacterService/ICharacterService.cs
+++ Controllers/Services/CharacterService/ICharacterService.cs
@@ -5,8 +5,8 @@ namespace dotnet_rpg.Controllers.Services.CharacterService
     using dotnet_rpg.Models;
     public interface ICharacterService
     {
-        Task<List<Character>> GetAllCharacters();
-        Task<Character> GetCharacterById(int id);
-        Task<List<Character>> AddCharacter(Character newCharacter);
+        Task<ServiceResponse<List<Character>>> GetAllCharacters();
+        Task<ServiceResponse<Character>> GetCharacterById(int id);
+        Task<ServiceResponse<List<Character>>> AddCharacter(Character newCharacter);
     }
 }
